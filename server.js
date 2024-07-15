5d95459b8fa04f2b6358c7e096155d544d8a4700
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-07-15 00:29:01 -0500

    routes dir has been created with products file. Same with Controllers dir and productController file. Server has been updated to use these new routes. This is a basic structure for managing products through the API.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+const productRoutes = require('./routes/products');
+
+app.use('/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
